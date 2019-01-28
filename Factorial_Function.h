b8c27c33fdf0fe39a08a202a420650de966509a1
newEndavour <36076458+newEndeavour@users.noreply.github.com>
2019-01-28 00:57:47 +0000

    Add files via upload

---
+++ Factorial_Function.h
@@ -0,0 +1,42 @@
+/*
+  File:         Factorial_Function.h
+  Version:      0.0.1
+  Date:         27-Jan-2019
+  Revision:     27-Jan-2019
+  Author:       Jerome Drouin (jerome.p.drouin@gmail.com)
+
+  Factorial_Function.h - Library for 'duino
+  https://github.com/newEndeavour/Factorial_Function
+
+  Factorial_Function implements the Mathematical Factorial function. 
+
+  Copyright (c) 2018-2019 Jerome Drouin  All rights reserved.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Editions:	
+	0.0.1	: first version
+	
+*/
+
+
+#ifndef FACTORIAL_FUNCTION_H
+#define FACTORIAL_FUNCTION_H
+
+double Factorial(int n);
+long double xFactorial(int n);
+int Factorial_Max_Arg(void);
+
+
+#endif
